74f7da542a78fdbd12fdc69dcd1f15956f33cf10
ikism <amlxss@163.com>
2022-09-25 22:38:28 +0800

    add dio fetch and cookie manager; init login account;

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,23 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:http/http.dart' as http;
-import 'package:html/parser.dart' show parse;
+
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:redux/redux.dart';
-import 'package:wallhevan/pictureComp.dart';
 import 'package:wallhevan/pictureViews.dart';
 import 'package:wallhevan/store/index.dart';
 import './picture.dart';
+import 'Account/account.dart';
+import 'Account/login.dart';
+import 'api.dart';
+
 
 void main() {
   if (Platform.isAndroid) {
     SystemChrome.setSystemUIOverlayStyle(
         const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   }
+
   Store<MainState> store = Store<MainState>(
     counterReducer,
     initialState: MainState.initState(),
@@ -25,140 +28,6 @@ void main() {
     ],
   );
   runApp(MyApp(store: store));
-}
-
-enum Actions {
-  addAllWallImage,
-  addAllWidget,
-  addWallImage,
-  addWidget,
-  changeIndex,
-  loading,
-  init,
-  preview,
-  selectBottomNav
-}
-
-MainState counterReducer(MainState state, dynamic action) {
-  switch (action['type']) {
-    case Actions.addAllWallImage:
-      state.imageDataList.addAll(action['data']);
-      break;
-    case Actions.addAllWidget:
-      state.imageList.addAll(action['data']);
-      break;
-    case Actions.addWallImage:
-      state.imageDataList.add(action['data']);
-      break;
-    case Actions.addWidget:
-      state.imageList.add(action['data']);
-      break;
-    case Actions.changeIndex:
-      state.currentIndex = action['data'];
-      break;
-    case Actions.loading:
-      state.loading = !state.loading;
-      break;
-    case Actions.init:
-      // getImage(action['context'], state);
-      break;
-    case Actions.selectBottomNav:
-      state.bottomNavIndex = action['data'];
-      break;
-    case Actions.preview:
-      state.currentIndex = action['currentIndex'];
-      state.preview = !state.preview;
-      break;
-  }
-  return state;
-}
-
-void fetchContactorMiddleware(
-    Store<MainState> store, action, NextDispatcher next) {
-  if (action['type'] == Actions.init) {
-    getImage(store, action['context']);
-  }
-  next(action);
-}
-
-void getImage(Store<MainState> store, BuildContext context) {
-  if (store.state.loading) return;
-  //https://wallhaven.cc/search?categories=010&purity=110&topRange=1M&sorting=toplist&order=desc
-  var params = {
-    'categories': '010',
-    'purity': '110',
-    'topRange': '1M',
-    'sorting': 'toplist',
-    'order': 'desc',
-    'page': store.state.pageNum.toString(),
-  };
-  var url = Uri.https('wallhaven.cc', 'search', params);
-  store.state.pageNum++;
-  store.state.loading = true;
-  http.get(url, headers: {'x-requested-with': 'XMLHttpRequest'}).then(
-      (response) {
-    store.state.loading = false;
-    var document = parse(response.body.replaceAll("/small/", "/orig/"));
-    var figureList = document.getElementsByTagName("figure");
-    List<Widget> list = [];
-    List<WallImage> dataList = [];
-    double halfWidth = MediaQuery.of(context).size.width / 2;
-    for (var element in figureList) {
-      var img = element.querySelector('img');
-      var href = element.querySelector('.preview');
-      var size = element.querySelector('.wall-res');
-      var png = element.querySelector('.png');
-      var obj = WallImage('');
-      img?.attributes.forEach((key, value) {
-        if (key == "data-src") {
-          obj.pSrc = value;
-        }
-      });
-      href?.attributes.forEach((key, value) {
-        if (key == "href") {
-          obj.href = value;
-        }
-      });
-      String nSize = (size?.innerHtml).toString();
-      List sizes = nSize.split(' x ');
-
-      if (sizes.isNotEmpty) {
-        double rWidth = double.parse(sizes[0]);
-        double rHeight = double.parse(sizes[1]);
-        double pHeight = rHeight / (rWidth / halfWidth);
-        obj.size = {
-          'pWidth': halfWidth,
-          'pHeight': pHeight,
-          'width': rWidth,
-          'height': rHeight
-        };
-        List<String> fullSrc = obj.pSrc
-            .replaceAll('/th.wallhaven.cc/orig/', '/w.wallhaven.cc/full/')
-            .split('/');
-        fullSrc[fullSrc.length - 1] =
-            "wallhaven-${fullSrc[fullSrc.length - 1]}";
-        if (png != null) {
-          fullSrc[fullSrc.length - 1] =
-              fullSrc[fullSrc.length - 1].replaceAll('.jpg', '.png');
-        }
-        obj.src = fullSrc.join('/');
-        // obj.pSrc = obj.src; // TODO be remove
-        debugPrint(obj.toString());
-        list.add(PictureComp(
-            pHeight: pHeight,
-            halfWidth: halfWidth,
-            image: obj,
-            type: WallImage.previewPicture));
-      } else {
-        list.add(Image.network(obj.pSrc, fit: BoxFit.scaleDown));
-      }
-      // print(obj);
-      dataList.add(obj);
-    }
-    // print(state.im)
-    store.dispatch({'type': Actions.addAllWidget, 'data': list});
-    store.dispatch({'type': Actions.addAllWallImage, 'data': dataList});
-  }).catchError((error) => {print(error.toString())});
 }
 
 class MyApp extends StatelessWidget {
@@ -185,12 +54,17 @@ class MyApp extends StatelessWidget {
               // is not restarted.
               primarySwatch: Colors.blue,
             ),
-            initialRoute: '/',
+            initialRoute: '/login',
             routes: {
               '/picture': (context) => const Picture(),
               '/pictureViews': (context) => const PictureViews(),
+              '/account': (context) => const Account(),
+              '/login': (context) => const Login(),
             },
             home: StoreBuilder<MainState>(
+              onInit: (store) {
+                initDio();
+              },
               builder: (BuildContext context, Store<MainState> store) =>
                   MyHomePage(store: store),
             )));
@@ -201,47 +75,59 @@ class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.store});
 
   final Store<MainState> store;
-  void _onItemTapped(int index){
-    store.dispatch({'type':Actions.selectBottomNav,'data':index});
+  void _onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        Navigator.pushNamed(context, '/');
+        break;
+      case 1:
+        Navigator.pushNamed(context, '/login');
+        break;
+      case 2:
+        Navigator.pushNamed(context, '/account');
+        break;
+    }
+    store.dispatch({'type': StoreActions.selectBottomNav, 'data': index});
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+    return ConstrainedBox(
+      constraints: const BoxConstraints.expand(),
+      child: Scaffold(
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.business),
+                label: 'Business',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.school),
+                label: 'School',
+              ),
+            ],
+            currentIndex: store.state.bottomNavIndex,
+            selectedItemColor: Colors.amber[800],
+            onTap: (index) => _onItemTapped(context, index),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ],
-        currentIndex: store.state.bottomNavIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
-      body: ConstrainedBox(
-          constraints: const BoxConstraints.expand(),
-          child: StoreConnector<MainState, MainState>(
+          body: StoreConnector<MainState, MainState>(
               // onWillChange: _onReduxChange,
               // onInitialBuild: _afterBuild,
               distinct: true,
               converter: (store) => store.state,
-              onInit: (store) =>
-                  store.dispatch({'type': Actions.init, 'context': context}),
+              onInit: (store) => store
+                  .dispatch({'type': StoreActions.init, 'context': context}),
               builder: (context, mainState) {
                 return NotificationListener<ScrollNotification>(
                   onNotification: (ScrollNotification scrollInfo) {
                     if (scrollInfo.metrics.pixels >=
                         scrollInfo.metrics.maxScrollExtent - 400) {
-                      store
-                          .dispatch({'type': Actions.init, 'context': context});
+                      store.dispatch(
+                          {'type': StoreActions.init, 'context': context});
                       return true;
                     }
                     return false;
@@ -254,7 +140,7 @@ class MyHomePage extends StatelessWidget {
                       return GestureDetector(
                           onTap: () => {
                                 store.dispatch({
-                                  'type': Actions.preview,
+                                  'type': StoreActions.preview,
                                   'currentIndex': index
                                 }),
                                 Navigator.pushNamed(context, '/pictureViews'),
--- lib/store/index.dart
+++ lib/store/index.dart
@@ -1,12 +1,71 @@
-
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-
+import 'package:redux/redux.dart';
+import 'package:html/parser.dart' show parse;
+import 'package:wallhevan/api.dart';
 import '../main.dart' show WallImage;
+import 'package:wallhevan/pictureComp.dart';
 
-enum Actions { increment }
+
+enum StoreActions {
+  addAllWallImage,
+  addAllWidget,
+  addWallImage,
+  addCookie,
+  changeIndex,
+  loading,
+  init,
+  preview,
+  selectBottomNav,
+  accountChange,
+}
+
+MainState counterReducer(MainState state, dynamic action) {
+  switch (action['type']) {
+    case StoreActions.addAllWallImage:
+      state.imageDataList.addAll(action['data']);
+      break;
+    case StoreActions.addAllWidget:
+      state.imageList.addAll(action['data']);
+      break;
+    case StoreActions.addWallImage:
+      state.imageDataList.add(action['data']);
+      break;
+    case StoreActions.addCookie:
+      state.cookie = (action['data']);
+      break;
+    case StoreActions.changeIndex:
+      state.currentIndex = action['data'];
+      break;
+    case StoreActions.loading:
+      state.loading = !state.loading;
+      break;
+    case StoreActions.init:
+      // getImage(action['context'], state);
+      break;
+    case StoreActions.selectBottomNav:
+      state.bottomNavIndex = action['data'];
+      break;
+    case StoreActions.preview:
+      state.currentIndex = action['currentIndex'];
+      state.preview = !state.preview;
+      break;
+    case StoreActions.accountChange:
+      state.account = action['data'];
+      break;
+  }
+  return state;
+}
+
+void fetchContactorMiddleware(
+    Store<MainState> store, action, NextDispatcher next) {
+  if (action['type'] == StoreActions.init) {
+    getImage(store, action['context']);
+  }
+  next(action);
+}
 
 class MainState {
-
   final List<Widget> imageList;
   final List<WallImage> imageDataList;
   bool preview;
@@ -14,18 +73,186 @@ class MainState {
   int pageNum;
   int currentIndex;
   int bottomNavIndex;
-
+  UserAccount account = UserAccount();
+  String cookie = '';
   MainState(this.imageList, this.imageDataList, this.preview, this.loading,
-      this.pageNum, this.currentIndex,this.bottomNavIndex);
+      this.pageNum, this.currentIndex, this.bottomNavIndex);
 
-  factory MainState.initState() => MainState([],[],false,false,1,0,0);
+  factory MainState.initState() => MainState([], [], false, false, 1, 0, 0);
 
-  void addWallImage(WallImage img){
+  void addWallImage(WallImage img) {
     imageDataList.add(img);
   }
 
-  void addPictureWidget(Widget widget){
+  void addPictureWidget(Widget widget) {
     imageList.add(widget);
   }
+}
 
+class HandleActions {
+  Store<MainState> store;
+
+  HandleActions(this.store);
+
+  bool loading = false;
+
+  void userNameChanged(String userName) {
+    store.state.account.username = userName;
+    store.dispatch(
+        {'type': StoreActions.accountChange, 'data': store.state.account});
+  }
+
+  void passwordChanged(String pwd) {
+    store.state.account.password = pwd;
+    store.dispatch(
+        {'type': StoreActions.accountChange, 'data': store.state.account});
+  }
+
+  void tokenChanged(String token) {
+    store.state.account.token = token;
+    store.dispatch(
+        {'type': StoreActions.accountChange, 'data': store.state.account});
+  }
+
+  void setCookie(String cookie) {
+    // cookie.split(';')
+    // var dc = Cookie();
+    // dio.interceptors.add(cookie);
+  }
+
+  void getToken() {
+    if (loading || store.state.account.token.isNotEmpty) return;
+    loading = true;
+    Map<String, String> header = {};
+    // if(store.state.cookie.isNotEmpty){
+    //   header =  {
+    //     'cookie':store.state.cookie
+    //   };
+    // }
+    dio.get('/login', options: Options(headers: header)).then((response) {
+      loading = false;
+      // Api.cookieJar.loadForRequest(uri)
+      // CookieManager.
+      // Api.getSetCookie(response: response);
+      var body = parse(response.data);
+      var input = body.querySelector('[name="_token"]');
+      var token = input?.attributes['value'];
+      tokenChanged(token!);
+    });
+  }
+
+  void login() {
+    UserAccount account = store.state.account;
+    dio.post('/auth/login', data: {
+      '_token': account.token,
+      'username': account.username,
+      'password': account.password,
+    }).then((response) {
+      Api.getSetCookie(response: response);
+      // print(response.headers['set-cookie']);
+    },onError: (error){
+      debugPrint(error);
+    }
+    );
+  }
+}
+
+class UserAccount {
+  String username = 'ikism';
+  String password = 'qpwoeiruty-1234';
+  String token = '';
+
+  @override
+  String toString() {
+    return 'UserAccount{userName: $username, password: $password, token: $token}';
+  }
+
+// UserAccount(this.userName, this.password, this.token);
+
+}
+
+void getImage(Store<MainState> store, BuildContext context) {
+  if (store.state.loading) return;
+  //https://wallhaven.cc/search?categories=010&purity=110&topRange=1M&sorting=toplist&order=desc
+  //https://wallhaven.cc/search?categories=010&purity=001&sorting=hot&order=desc
+  var params = {
+    'categories': '010',
+    'purity': '001',
+    'topRange': '1M',
+    'sorting': 'toplist',
+    'order': 'desc',
+    'page': store.state.pageNum.toString(),
+  };
+  store.state.pageNum++;
+  store.state.loading = true;
+  dio
+      .get('/search',
+          queryParameters: params,
+          options: Options(headers: {'x-requested-with': 'XMLHttpRequest'}))
+      .then((response) {
+    store.state.loading = false;
+
+    // Api.getSetCookie(response: response);
+
+    var document = parse(response.data.replaceAll("/small/", "/orig/"));
+    var figureList = document.getElementsByTagName("figure");
+    List<Widget> list = [];
+    List<WallImage> dataList = [];
+    double halfWidth = MediaQuery.of(context).size.width / 2;
+    for (var element in figureList) {
+      var img = element.querySelector('img');
+      var href = element.querySelector('.preview');
+      var size = element.querySelector('.wall-res');
+      var png = element.querySelector('.png');
+      var obj = WallImage('');
+      img?.attributes.forEach((key, value) {
+        if (key == "data-src") {
+          obj.pSrc = value;
+        }
+      });
+      href?.attributes.forEach((key, value) {
+        if (key == "href") {
+          obj.href = value;
+        }
+      });
+      String nSize = (size?.innerHtml).toString();
+      List sizes = nSize.split(' x ');
+
+      if (sizes.isNotEmpty) {
+        double rWidth = double.parse(sizes[0]);
+        double rHeight = double.parse(sizes[1]);
+        double pHeight = rHeight / (rWidth / halfWidth);
+        obj.size = {
+          'pWidth': halfWidth,
+          'pHeight': pHeight,
+          'width': rWidth,
+          'height': rHeight
+        };
+        List<String> fullSrc = obj.pSrc
+            .replaceAll('/th.wallhaven.cc/orig/', '/w.wallhaven.cc/full/')
+            .split('/');
+        fullSrc[fullSrc.length - 1] =
+            "wallhaven-${fullSrc[fullSrc.length - 1]}";
+        if (png != null) {
+          fullSrc[fullSrc.length - 1] =
+              fullSrc[fullSrc.length - 1].replaceAll('.jpg', '.png');
+        }
+        obj.src = fullSrc.join('/');
+        // obj.pSrc = obj.src; // TODO be remove
+        // debugPrint(obj.toString());
+        list.add(PictureComp(
+            pHeight: pHeight,
+            halfWidth: halfWidth,
+            image: obj,
+            type: WallImage.previewPicture));
+      } else {
+        list.add(Image.network(obj.pSrc, fit: BoxFit.scaleDown));
+      }
+      // print(obj);
+      dataList.add(obj);
+    }
+    // print(state.im)
+    store.dispatch({'type': StoreActions.addAllWidget, 'data': list});
+    store.dispatch({'type': StoreActions.addAllWallImage, 'data': dataList});
+  }).catchError((error) => {print(error.toString())});
 }
